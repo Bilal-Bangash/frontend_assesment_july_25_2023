58c7b80d754d852905911ac182370f17eaf7bdd5
Bilal <mbilal@sirma.com>
2023-07-25 19:58:46 +0500

    added product detail page

--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -1,4 +1,7 @@
 import {
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
@@ -16,6 +19,25 @@ export const listProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const detailProducts = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST })
+    const { data } = await api.get(`/products/${id}`)
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- src/screens/ProductScreen.jsx
+++
@@ -1,5 +0,0 @@
-const ProductScreen = () => {
-  return <>ProductScreen</>
-}
-
-export default ProductScreen
